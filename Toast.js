4fef9fb3ee1cff5b619ee5fa2bc990e692278071
EbenauDev <ebenau06@gmail.com>
2022-03-19 19:24:50 -0300

    Melhorado estilo da página inicial

--- Toast.js
+++ Toast.js
@@ -1,33 +1,40 @@
 const configuration = { position: "top-right", delay: 3000 };
 let toastContainerElement;
 
-
-const toast = {
-    success: (message, title) => {
-
-
-    },
-    error: (message, tittle) => {
-
-    }
-}
-
-
 const _createToasterContainer = () => {
     toastContainerElement = document.createElement("div");
-    toastContainerElement.setAttribute("data-postion", configuration.position);
+    toastContainerElement.classList.add("toast-container");
+    toastContainerElement.setAttribute("data-position", configuration.position);
     document.body.appendChild(toastContainerElement);
-    console.log(toastContainerElement);
+}
+
+const _createToaster = (message, type) => {
+    const toaster = document.createElement("div");
+    toaster.classList.add("toast");
+    toaster.classList.add(type);
+    toaster.innerText = message;
+    toaster.onclick = () => { toaster.remove() };
+    toastContainerElement.appendChild(toaster);
+    setTimeout(() => {
+        toaster.remove();
+    }, configuration.delay);
+}
+
+const toast = {
+    success: (message) => {
+        _createToaster(message, "toast-success");
+    },
+    error: (message) => {
+        _createToaster(message, "toast-error");
+    }
 }
 
 const toastProvider = {
     setPosition: (value) => {
         configuration["position"] = value;
-        window.localStorage.setItem("ToastConfig", JSON.stringify(configuration));
     },
     setDelay: (value) => {
         configuration["delay"] = value;
-        window.localStorage.setItem("ToastConfig", JSON.stringify(configuration));
     },
     init() {
         _createToasterContainer();
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 import { toast, toastProvider } from "./Toast.js";
 
+
+toastProvider.setDelay(50000);
 toastProvider.init();
+
+// setTimeout(() => {
+//     toast.success("Sou o primeiro toast");
+// }, 1000);
+
+// setTimeout(() => {
+//     toast.success("Sou o segundo toast");
+// }, 2000);
+
+// setTimeout(() => {
+//     toast.success("Sou o terceiro toast");
+// }, 3000);
+
